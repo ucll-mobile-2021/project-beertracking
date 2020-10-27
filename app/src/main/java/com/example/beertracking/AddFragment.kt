78260aeb9557b6e89180a7e2412290c39e46bfd8
ThijsVlaeyen <thijs.vlaeyen@student.ucll.be>
2020-10-27 14:59:56 +0100

    testing

---
+++ app/src/main/java/com/example/beertracking/AddFragment.kt
@@ -0,0 +1,24 @@
+package com.example.beertracking
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+
+class AddFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?{
+        setHasOptionsMenu(true)
+        return inflater.inflate(R.layout.fragment_add, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController())
+                || super.onOptionsItemSelected(item)
+    }
+}
